4adb5333f3ea82c8dd5ff2ae461425b4b3c18939
HibaEssamih <101391898+HibaEssamih@users.noreply.github.com>
2026-02-14 15:42:10 +0100

    ok

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,12 +19,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap" rel="stylesheet" />
       </head>
-      <body className={`${inter.variable} antialiased font-display`}>
+      <body className={`${inter.variable} antialiased font-display`} suppressHydrationWarning>
         {children}
       </body>
     </html>
